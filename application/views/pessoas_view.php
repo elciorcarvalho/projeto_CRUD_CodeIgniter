81a23946a9607a1157d83b330ec55eee5475eef6
Elcior Santana <elciorfscarvalho@gmail.com>
2017-02-03 17:01:48 -0300

    Criado o primeiro view

---
+++ application/views/pessoas_view.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+*Este arquivo será a visão, 
+*uma página no formato HTML que 
+*fará a interface com o usuário (apresentação).
+*/
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title><?php echo $titulo; ?></title>
+    <meta charset="UTF-8" />
+</head>
+<body>
+   <h1>Parabéns! Você já deu o primeiro passo.</h1>
+</body>
+</html>
