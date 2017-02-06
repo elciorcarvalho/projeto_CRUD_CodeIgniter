b881a182cf20b84249e3cfb1e4515665f3cb4370
elciorcarvalho <elciorfscarvalho@gmail.com>
2017-02-06 13:38:19 -0200

    Adiciona o modelo para fazer as requisições ao banco de dados

--- application/models/Pessoas_model.php
+++ application/models/Pessoas_model.php
@@ -5,3 +5,21 @@
 *a classe responsável por gravar 
 *e recuperar os dados do banco.
 */
+class Pessoas_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function inserir($data)
+	{
+		return $this->db->inserir('pessoas', $data);
+	}
+
+	function listar()
+	{
+		$query = $this->db->get('pessoas');
+		return $query->result();
+	}
+}
